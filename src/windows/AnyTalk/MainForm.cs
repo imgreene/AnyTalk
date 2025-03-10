810896f1e06c2e062a48b28e0ed158e6d6d23ce3
Chase <chase@MacBook-Pro.local>
2025-03-10 13:02:03 -0600

    Add Windows project structure

---
+++ src/windows/AnyTalk/MainForm.cs
@@ -0,0 +1,15 @@
+namespace AnyTalk;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        this.Text = "AnyTalk";
+        this.Size = new Size(400, 300);
+    }
+}
