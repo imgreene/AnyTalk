3e3cb6a15c3237d18fb3426888faac2908ad81bf
Chase <chase@MacBook-Pro.local>
2025-03-10 18:15:51 -0600

    build error

--- src/windows/AnyTalk/Services/HotkeyManager.cs
+++ src/windows/AnyTalk/Services/HotkeyManager.cs
@@ -2,55 +2,68 @@ using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
-public class HotkeyManager : IDisposable
+namespace AnyTalk
 {
-    private bool _isRegistered;
-    private readonly int _hotkeyId;
-    private readonly IntPtr _handle;
-
-    [DllImport("user32.dll")]
-    private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
-
-    [DllImport("user32.dll")]
-    private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-    private static extern IntPtr GetModuleHandle(string? lpModuleName);
-
-    public HotkeyManager(IntPtr handle, int hotkeyId)
+    [Flags]
+    public enum ModifierKeys : uint
     {
-        _handle = handle;
-        _hotkeyId = hotkeyId;
-        _isRegistered = false;
+        None = 0x0000,
+        Alt = 0x0001,
+        Control = 0x0002,
+        Shift = 0x0004,
+        Win = 0x0008
     }
 
-    public bool RegisterHotkey(Keys key, ModifierKeys modifiers)
+    public class HotkeyManager : IDisposable
     {
-        if (_isRegistered)
+        private bool _isRegistered;
+        private readonly int _hotkeyId;
+        private readonly IntPtr _handle;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetModuleHandle(string? lpModuleName);
+
+        public HotkeyManager(IntPtr handle, int hotkeyId)
+        {
+            _handle = handle;
+            _hotkeyId = hotkeyId;
+            _isRegistered = false;
+        }
+
+        public bool RegisterHotkey(Keys key, ModifierKeys modifiers)
+        {
+            if (_isRegistered)
+            {
+                UnregisterHotkey();
+            }
+
+            _isRegistered = RegisterHotKey(_handle, _hotkeyId, (uint)modifiers, (uint)key);
+            return _isRegistered;
+        }
+
+        public void UnregisterHotkey()
+        {
+            if (_isRegistered)
+            {
+                UnregisterHotKey(_handle, _hotkeyId);
+                _isRegistered = false;
+            }
+        }
+
+        public static IntPtr GetModuleHandleForCurrentProcess()
+        {
+            return GetModuleHandle(null);
+        }
+
+        public void Dispose()
         {
             UnregisterHotkey();
         }
-
-        _isRegistered = RegisterHotKey(_handle, _hotkeyId, (uint)modifiers, (uint)key);
-        return _isRegistered;
-    }
-
-    public void UnregisterHotkey()
-    {
-        if (_isRegistered)
-        {
-            UnregisterHotKey(_handle, _hotkeyId);
-            _isRegistered = false;
-        }
-    }
-
-    public static IntPtr GetModuleHandleForCurrentProcess()
-    {
-        return GetModuleHandle(null);
-    }
-
-    public void Dispose()
-    {
-        UnregisterHotkey();
     }
 }
